0c941965fcdfefa7d7da5d4164b483010dc61741
emersonlj03 <emontenegromadrid1@gmail.com>
2025-10-12 21:03:58 -0500

    edicion de encabezado - quitar carousel chica gym

--- Views/template/header-principal.php
+++ Views/template/header-principal.php
@@ -37,9 +37,9 @@ if (session_status() === PHP_SESSION_NONE) {
         <div class="w-100 d-flex justify-content-between">
             <div>
                 <i class="fa fa-envelope mx-2"></i>
-                <a class="navbar-sm-brand text-light text-decoration-none" href="mailto:info@company.com">info@company.com</a>
+                <a class="navbar-sm-brand text-light text-decoration-none" href="mailto:info@company.com">urbancloud4@gmail.com</a>
                 <i class="fa fa-phone mx-2"></i>
-                <a class="navbar-sm-brand text-light text-decoration-none" href="tel:010-020-0340">010-020-0340</a>
+                <a class="navbar-sm-brand text-light text-decoration-none" href="tel:010-020-0340">+51 987 654 321</a>
             </div>
             <div>
                 <a class="text-light" href="https://fb.com/templatemo" target="_blank" rel="sponsored"><i class="fab fa-facebook-f fa-sm fa-fw me-2"></i></a>
